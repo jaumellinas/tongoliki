d04f43cb4a0c4be94a8385076831c4c8269067a0
Daniel Cobo <danielpro1209@gmail.com>
2025-04-01 18:54:52 +0200

    persona_admin with add-delete-edit added

--- app.py
+++ app.py
@@ -32,6 +32,35 @@ def get_personas():
     personas = Persona.query.all()
     return render_template("personas.html", personas = personas)
 
+@app.route('/personas_admin')
+def get_personas_admin():
+    personas = Persona.query.all()
+    return render_template("personas_admin.html", personas = personas)
+
+@app.route('/editar_persona/<int:id>', methods=['GET', 'POST'])
+def editar_persona(id):
+    persona = Persona.query.get(id)
+    if not persona:
+        return "Usuario no encontrado", 404
+    if request.method == 'POST':
+        persona.name = request.form.get('name')
+        persona.surname = request.form.get('surname')
+        persona.age = int(request.form.get('age'))
+        persona.is_trainer = request.form.get('is_trainer') == 'on'
+        db.session.commit()
+        return redirect(url_for('get_personas'))
+    
+    return render_template("editar_persona.html", persona = persona)
+
+@app.route('/borrar_persona/<int:id>', methods=['GET', 'POST'])
+def borrar_persona(id):
+    persona = Persona.query.get(id)
+    if not persona:
+        return "Usuario no encontrado", 404
+    db.session.delete(persona)
+    db.session.commit()
+    return redirect(url_for('get_personas_admin'))
+
 @app.route('/add_persona', methods=['GET', 'POST'])
 def add_persona():
     if request.method == 'POST':
